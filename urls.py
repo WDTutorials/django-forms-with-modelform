f8a37449ce06dd2a870f9fcd14c1c2121e5463a3
Samuli Natri <samuli.natri@gmail.com>
2017-09-27 17:13:22 +0300

    Initial

---
+++ urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^add/blog/$', views.add_blog, name='add_blog'),
+    url(r'^edit/blog/(?P<id>\d+)/$', views.edit_blog, name='edit_blog'),
+    url(r'^blog/(?P<id>\d+)/$', views.blog, name='blog'),
+]
